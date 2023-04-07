448c1f6bb7be5ce57c20b99b41088aca0e2f0afb
Glad225 <okorochaglad170@gmail.com>
2023-04-07 14:48:42 -0700

    Task 1

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+/**
+ * hash_djb2 - Hashes a string using the djb2 algorithm
+ * @str: The string to be hashed
+ *
+ * Return: The hash value of the string as an unsigned long int
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 5381;
+	unsigned int c;
+
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+
+	return (hash);
+}
